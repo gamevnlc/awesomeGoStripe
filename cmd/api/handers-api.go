48a64ff79add914667d5b32a7bfda1b94eaaffac
Pham Xuan Huy <huypx@gearinc.com>
2025-04-25 16:40:00 +0700

    06-011 Generating and sending back a token

--- cmd/api/handers-api.go
+++ cmd/api/handers-api.go
@@ -279,16 +279,21 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	// Generate token
-
+	token, err := models.GenerateToken(user.ID, 24*time.Hour, models.ScopeAuthentication)
+	if err != nil {
+		app.badRequest(w, r, err)
+	}
 	// Send response
 
 	var payload struct {
-		Error   bool   `json:"error"`
-		Message string `json:"message"`
+		Error   bool          `json:"error"`
+		Message string        `json:"message"`
+		Token   *models.Token `json:"authentication_token"`
 	}
 
 	payload.Error = false
-	payload.Message = "Success!"
+	payload.Message = fmt.Sprintf("token for %s created", userInput.Email)
+	payload.Token = token
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
 }
