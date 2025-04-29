fc59882e93d254cdb9a02de7ce706a9947e51107
Pham Xuan Huy <huypx@gearinc.com>
2025-04-29 10:30:30 +0700

    08-002 Sending mail Part I

---
+++ cmd/api/mailer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"html/template"
+)
+
+//go:embed templates
+var emailTemplateFS embed.FS
+
+func (app *application) SendMail(from, to, subject, tmpl string, data interface{}) error {
+	templateToRender := fmt.Sprintf("templates/%s.html.tmpl", tmpl)
+
+	t, err := template.New("email-html").ParseFS(emailTemplateFS, templateToRender)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	var tpl bytes.Buffer
+	err = t.ExecuteTemplate(&tpl, "body", data)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	formattedMessage := tpl.String()
+
+	templateToRender = fmt.Sprintf("templates/%s.plain.tmpl", tmpl)
+
+	t, err = template.New("email-plain").ParseFS(emailTemplateFS, templateToRender)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	err = t.ExecuteTemplate(&tpl, "body", formattedMessage)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	plainMessage := tpl.String()
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -22,6 +22,8 @@ require (
 	github.com/jackc/pgx/v5 v5.5.4 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
+	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
